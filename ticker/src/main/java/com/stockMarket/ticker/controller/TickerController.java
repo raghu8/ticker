4bb7cc98882ee797a9d5f13a4e124e31dbd057d5
raghu8 <rvmanthena@gmail.com>
2020-11-16 22:03:25 -0500

    ticker

---
+++ ticker/src/main/java/com/stockMarket/ticker/controller/TickerController.java
@@ -0,0 +1,10 @@
+package com.stockMarket.ticker.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TickerController {
+	
+	//TODO: Create rest end point to read stock ticker information.
+	
+}
